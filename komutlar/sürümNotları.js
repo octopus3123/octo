e1d27bdbdefd52d0e5423ff4a3e32461bc1420f0
octopus3123 <42851843+octopus3123@users.noreply.github.com>
2018-09-01 01:04:19 +0300

    Update sürümNotları.js

--- komutlar/sürümNotları.js
+++ komutlar/sürümNotları.js
@@ -12,11 +12,11 @@ var surum = main.surum;
 exports.run = (Octopus, message) => {
   message.channel.send({embed: {
                 author: {
-                    name: `Octopus Güncel Sürüm Notları (Sürüm: 2.0.0 > ${surum})`,
+                    name: `R4dar Güncel Sürüm Notları (Sürüm: 2.0.0 > ${surum})`,
                     icon_url: "https://cdn.discordapp.com/attachments/460788815078883328/474400706065989643/octo.png"
                   },
                 color: 0xD97634,
-                description: `**v2.0.0:** Yeni Müzik Komutları *(${prefix}müzik)*, Çeşitli Hata Düzeltmeleri \n**v2.0.1:** Müzik Komutlarındaki Ufak Hatalar Giderildi \n**v2.0.2:** ${prefix}ping Komutu Octopusun Trafiğini Gösterdiğinden Octopus İçin Yeniden Tasarlandı, Giriş Mesajı Yeniden Düzenlendi, Ufak Hatalar Düzeltildi, ${prefix}sürümNotları Komutu Düzenlendi`
+                description: `**v2.0.0:** Yeni Müzik Komutları *(${prefix}müzik)*, Çeşitli Hata Düzeltmeleri \n**v2.0.1:** Müzik Komutlarındaki Ufak Hatalar Giderildi \n**v2.0.2:** ${prefix}ping Komutu Octopusun Trafiğini Gösterdiğinden Octopus İçin Yeniden Tasarlandı, Giriş Mesajı Yeniden Düzenlendi, Ufak Hatalar Düzeltildi, ${prefix}sürümNotları Komutu Düzenlendi\n Bot Adı **R4dar** Olarak Değiştirildi`
               }});
 };
 
